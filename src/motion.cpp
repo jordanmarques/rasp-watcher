b3b4fe6de57e6d11861c1d91052061920c13814f
Jordan Marques <jordanmarques@users.noreply.github.com>
2016-07-21 12:39:01 +0200

    clean

--- src/motion.cpp
+++ src/motion.cpp
@@ -13,19 +13,19 @@
 using namespace std;
 using namespace cv;
 
-// Check if the directory exists, if not create it
-// This function will create a new directory if the image is the first
+// Vérifie si le repo existe
+// Créer un nouveau repo si l'image est la première
 inline void directoryExistsOrCreate(const char* pzPath)
 {
     DIR *pDir;
-    // directory doesn't exists -> create it
+    // si repo existe pas, création
     if ( pzPath == NULL || (pDir = opendir (pzPath)) == NULL)
         mkdir(pzPath, 0777);
     else if(pDir != NULL)
         (void) closedir (pDir);
 }
 
-// When motion is detected we write the image to disk
+// quand un mouvement est detecté on la sauvegarde
 int incr = 0;
 inline bool saveImg(Mat image, const string DIRECTORY, const string EXTENSION, const char * DIR_FORMAT, const char * FILE_FORMAT)
 {
@@ -34,48 +34,41 @@ inline bool saveImg(Mat image, const string DIRECTORY, const string EXTENSION, c
     struct tm * timeinfo;
     char TIME[80];
     time (&seconds);
-    // Get the current time
     timeinfo = localtime (&seconds);
     
-    // Create name for the directory
     strftime (TIME,80,DIR_FORMAT,timeinfo);
     ss.str("");
     ss << DIRECTORY << TIME;
     directoryExistsOrCreate(ss.str().c_str());
-    //ss << "/cropped";
-    //directoryExistsOrCreate(ss.str().c_str());
-
-    // Create name for the image
+   
+    // nom pour l'image
     strftime (TIME,80,FILE_FORMAT,timeinfo);
     ss.str("");
-    if(incr < 100) incr++; // quick fix for when delay < 1s && > 10ms, (when delay <= 10ms, images are overwritten)
+    if(incr < 100) incr++;
     else incr = 0;
     ss << DIRECTORY << TIME << static_cast<int>(incr) << EXTENSION;
     return imwrite(ss.str().c_str(), image);
 }
 
-// Check if there is motion in the result matrix
-// count the number of changes and return.
+// Vérifie si il y a des mouvements dans la matrice résultante
+// Compte le nombres de changements et le retourne
 inline int detectMotion(const Mat & motion, Mat & result, Mat & result_cropped,
                  int x_start, int x_stop, int y_start, int y_stop,
                  int max_deviation,
                  Scalar & color)
 {
-    // calculate the standard deviation
     Scalar mean, stddev;
     meanStdDev(motion, mean, stddev);
-    // if not to much changes then the motion is real (neglect agressive snow, temporary sunlight)
+    // si pas assez de changement, alors c'est l'image originel
     if(stddev[0] < max_deviation)
     {
         int number_of_changes = 0;
         int min_x = motion.cols, max_x = 0;
         int min_y = motion.rows, max_y = 0;
-        // loop over image and detect changes
+        // parcours l'image pour détecter les changements
         for(int j = y_start; j < y_stop; j+=2){ // height
             for(int i = x_start; i < x_stop; i+=2){ // width
-                // check if at pixel (j,i) intensity is equal to 255
-                // this means that the pixel is different in the sequence
-                // of images (prev_frame, current_frame, next_frame)
+                // si l'intensité est égale a 255, le pixel est différent
                 if(static_cast<int>(motion.at<uchar>(j,i)) == 255)
                 {
                     number_of_changes++;
@@ -87,7 +80,7 @@ inline int detectMotion(const Mat & motion, Mat & result, Mat & result_cropped,
             }
         }
         if(number_of_changes){
-            //check if not out of bounds
+            //check si on est pas en dehors des limites
             if(min_x-10 > 0) min_x -= 10;
             if(min_y-10 > 0) min_y -= 10;
             if(max_x+10 < result.cols-1) max_x += 10;
@@ -101,70 +94,60 @@ inline int detectMotion(const Mat & motion, Mat & result, Mat & result_cropped,
 
 int main (int argc, char * const argv[])
 {
-    const string DIR = "/home/pi/Projects/raspwatcher/pics"; // directory where the images will be stored
-    const string EXT = ".jpg"; // extension of the images
-    const int DELAY = 100; // in mseconds, take a picture every 1/2 second
+    const string DIR = "/home/pi/Projects/raspwatcher/pics";
+    const string EXT = ".jpg";
+    const int DELAY = 100; 
     const string LOGFILE = "/home/pi/Projects/raspwatcher/logs";
 
     // Format of directory
-    string DIR_FORMAT = ""; // 1Jan1970
+    string DIR_FORMAT = "";
     string FILE_FORMAT = DIR_FORMAT + "/" + "%d%h%Y_%H%M%S"; // 1Jan1970/1Jan1970_12153
 
-    // Set up camera
+    // configure la caméra
     VideoCapture camera(0);
-    if(!camera.isOpened())  // check if we succeeded
+    if(!camera.isOpened())
             return -1;
     std::cout << "Camera ok Début de la capture..." << std::endl;
     camera.set(CV_CAP_PROP_FRAME_WIDTH, 800);
     camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
     
-    // Take images and convert them to gray
+    // prend une photo et la converti en gris
     Mat result, result_cropped, current_frame, next_frame;
     camera.read(result);
     Mat prev_frame = result;
     camera.read(current_frame);
-    //Mat current_frame = cvQueryFrame(camera);
     camera.read(next_frame);
-    //Mat next_frame = cvQueryFrame(camera);
     cvtColor(current_frame, current_frame, CV_RGB2GRAY);
     cvtColor(prev_frame, prev_frame, CV_RGB2GRAY);
     cvtColor(next_frame, next_frame, CV_RGB2GRAY);
     
-    // d1 and d2 for calculating the differences
-    // result, the result of and operation, calculated on d1 and d2
-    // number_of_changes, the amount of changes in the result matrix.
-    // color, the color for drawing the rectangle when something has changed.
     Mat d1, d2, motion;
     int number_of_changes, number_of_sequence = 0;
     Scalar mean_, color(0,255,255); // yellow
     
-    // Detect motion in window
     int x_start = 10, x_stop = current_frame.cols-11;
     int y_start = 350, y_stop = 530;
 
-    // If more than 'there_is_motion' pixels are changed, we say there is motion
-    // and store an image on disk
+    // Si il y a plus de pixel changé que there_is_motion, il ya a un mouvement
+    // et sauvegarde l'image sur le disque
     int there_is_motion = 5;
     
-    // Maximum deviation of the image, the higher the value, the more motion is allowed
+    // plus la déviation est elevé plus le seuil de detcion l'est aussi
     int max_deviation = 20;
     
-    // Erode kernel
     Mat kernel_ero = getStructuringElement(MORPH_RECT, Size(2,2));
     
-    // All settings have been set, now go in endless loop and
-    // take as many pictures you want..
+    //Début de la boucle inifnie 
     while (true){
-        // Take a new image
+        //prend une photo
         prev_frame = current_frame;
         current_frame = next_frame;
         camera.read(next_frame);
-        //next_frame = cvQueryFrame(camera);
         result = next_frame;
         cvtColor(next_frame, next_frame, CV_RGB2GRAY);
 
-        // Calc differences between the images and do AND-operation
-        // Low differences are ignored (ex. contrast change due to sunlight)
+        // Calcule la différence entre les images en faisant un and
+        // de ce fait les faible difference de contrast sont ignorés
         absdiff(prev_frame, next_frame, d1);
         absdiff(next_frame, current_frame, d2);
         bitwise_and(d1, d2, motion);
@@ -173,7 +156,7 @@ int main (int argc, char * const argv[])
         
         number_of_changes = detectMotion(motion, result, result_cropped,  x_start, x_stop, y_start, y_stop, max_deviation, color);
         
-        // something changed.
+        // changement
         if(number_of_changes>=there_is_motion)
         {
             if(number_of_sequence>0){ 
@@ -185,7 +168,6 @@ int main (int argc, char * const argv[])
         else
         {
             number_of_sequence = 0;
-            //wait
             cvWaitKey (DELAY);
         }
     }
